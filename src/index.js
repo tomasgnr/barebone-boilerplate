9fb64436667d4bd441c98fade8731b5b4bab039d
tomasgnr <tomasgnr@gmail.com>
2021-11-02 19:19:52 +0100

    Clean up

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-const Index = () => {
-  return <div>Welcome to the app!</div>
-}
-ReactDOM.render(<Index />, document.getElementById('app'))
